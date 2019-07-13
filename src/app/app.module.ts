228d42fbccb8a1e9d89dc5c4274a4069935cbfe0
yuva951dev <yuva951.dev@gmail.com>
2019-07-13 14:45:00 +0530

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { AngularFireModule } from '@angular/fire';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
---
+++ src/environments/environments.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDZWL5hT8xIBWwBtTmmB9n8TLQn0MCgZ3g",
+    authDomain: "backofficeapp-d8f59.firebaseapp.com",
+    databaseURL: "https://backofficeapp-d8f59.firebaseio.com",
+    projectId: "backofficeapp-d8f59",
+    storageBucket: "backofficeapp-d8f59.appspot.com",
+    messagingSenderId: "599224163948",
+    appId: "1:599224163948:web:96ca7f217fa5f53b"
+  }
+};
