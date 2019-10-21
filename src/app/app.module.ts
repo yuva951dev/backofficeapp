1a5b2fea174777a80a6a52244995a969d9535aed
yuva951dev <yuva951.dev@gmail.com>
2019-10-21 18:19:06 +0530

    33 binaries uploaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from "@angular/fire/auth";
 import { environment } from '../environments/environment';
 import { HelloComponent } from './hello.component';
 import { EmployeeService } from './employee.service';
@@ -13,15 +14,16 @@ import { MoneySpendListComponent } from './money-spend-list/money-spend-list.com
 import { AccountsService } from './Services/accounts.service';
 import { MoneySpentService } from './Services/money-spent.service';
 import { routes } from './app.routes';
+import { AuthService } from './Services/auth.service';
 
 @NgModule({
   imports:      [ BrowserModule,
                   FormsModule,
                   RouterModule.forRoot(routes),
                   AngularFireModule.initializeApp(environment.firebase),
-                   AngularFirestoreModule ],
+                   AngularFirestoreModule,AngularFireAuthModule ],
   declarations: [ AppComponent, HelloComponent, AccountsComponent, MoneySpendListComponent ],
   bootstrap:    [ AppComponent ],
-  providers: [EmployeeService, AccountsService, MoneySpentService]
+  providers: [EmployeeService, AccountsService, MoneySpentService, AuthService]
 })
 export class AppModule { }
